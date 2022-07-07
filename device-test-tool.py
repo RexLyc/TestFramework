cab7d7218f01ccc4d334980a6224e1034f957dbc
liyicheng <liyicheng@bmncc.com.cn>
2022-07-07 19:00:17 +0800

    basically finish

--- device-test-tool.py
+++ device-test-tool.py
@@ -1,7 +1,15 @@
+from cgitb import reset
+from msilib.schema import Error
+from operator import ne, xor
+from platform import node
+from struct import unpack
 import sys
 import glob
+from numpy import isin
 import serial
 import time
+import json
+import idcard_sam_trans_model as model;
 
 def serial_ports():
     """ Lists serial port names
@@ -31,33 +39,273 @@ def serial_ports():
             pass
     return result
 
-def open_port(port,baudrate,parity):
-    return serial.Serial(port=port,baudrate=baudrate,parity=parity)
+class RunToolFunc:
+    def xorFunc(data):
+        # calculate xor
+        xorValue=0
+        for i in data:
+            xorValue^=i
+        return xorValue
 
-def send_str(port_set, baudrate, parity, value):
-    return port_set.write(value)
+    def getFunc(name):
+        # 将tpu命令打包（添加首尾、校验位、转义字符）
+        def pack_tpu(data):
+            # 添加异或校验字节
+            data.append(RunToolFunc.xorFunc(data))
+            # 添加转义字符0x10
+            result = []
+            for i in data:
+                if i==0x10 or i==0x02 or i==0x03:
+                    result.append(0x10)
+                result.append(i)
+            result.append(0x03)
+            result.insert(0,0x02)
+            return result
 
-def read_str(port_set,wait_time):
-    time.sleep(wait_time)
-    return port_set.read_all()
+        # 将tpu报文解包（去掉首尾、校验位、转义字符）
+        def unpack_tpu(data):
+            find_10=False
+            unpack_data=[]
+            for i in data:
+                if find_10:
+                    unpack_data.append(i)
+                    find_10=False
+                    continue
+                elif i == 0x10:
+                    find_10=True
+                else:
+                    unpack_data.append(i)
+            xorValue=unpack_data[-2]
+            unpack_data=unpack_data[1:-2]
+            checkValue=RunToolFunc.xorFunc(unpack_data)
+            if checkValue!=xorValue:
+                raise NameError("fatal error! xorValue invalid")
+            return unpack_data
 
-def run_test_graph():
+        # 打印数据（pre阶段打印输入，post阶段打印输出）
+        def log(data):
+            print(data)
+            return data
+        
+        def sam_to_tpu(data):
+            # 注意位移运算符优先级极低
+            sam_length=(data[1]<<8)+data[2]
+            if len(data) != sam_length +3:
+                raise NameError("fatal error! receive wrong length packet from sam")
+            result=[]
+            sam_length-=1
+            result.extend([0x32,0x54,0x00,0x20,0x22,0x07,0x07,0x14,0x11,0x00])
+            # Moto 序
+            result.append((sam_length&0xff00)>>8)
+            result.append(sam_length&0x00ff)
+            result.extend(data[4:])
+            total_length=len(result)
+            result.insert(0,(total_length&0xff00)>>8)
+            result.insert(0,total_length&0x00ff)
+            return result
+
+        def tpu_to_sam(data):
+            sam_length=(data[12]<<8)+data[13]
+            print(sam_length+12,len(data))
+            if len(data) != int(sam_length+14):
+                raise NameError("fatal error! receive wrong length packet from tpu")
+            result=[]
+            sam_length+=1
+            result.append((sam_length&0xff00)>>8)
+            result.append(sam_length&0x00ff)
+            result.append(0x00)
+            result.extend(data[14:])
+            return result
+        def bytes_to_list(data):
+            if isinstance(data,bytes):
+                temp = []
+                temp.extend(data) 
+                data=temp
+            return data
+
+        if name=='pack_tpu':
+            return pack_tpu
+        elif name=='unpack_tpu':
+            return unpack_tpu
+        elif name=='log':
+            return log
+        elif name=='sam_to_tpu':
+            return sam_to_tpu
+        elif name=='tpu_to_sam':
+            return tpu_to_sam
+        elif name=='bytes_to_list':
+            return bytes_to_list
+        else:
+            return None
+
+class Node:
+    def __init__(self,data_zone=None,node=None):
+        self.data_zone=data_zone
+        self.node=node
+
+    def __str__(self):
+        return 'Type: {}, Id: {}'.format(self.node['type'],self.node['id'])
+
+    # pre仅支持对in数据预处理，其结果影响全局
+    def _preRun(self):
+        if self.node is not None and 'preRun' in self.node:
+            for i in self.node['preRun']:
+                print('preRun: {}'.format(i))
+                self.data_zone[self.node['in']]=RunToolFunc.getFunc(i)(self.data_zone[self.node['in']])
+
+    def _run(self):
+        pass
+
+    # post仅支持对out数据后处理，其结果影响全局
+    def _postRun(self):
+        # 默认进行bytes到list的转换
+        if self.node is not None and 'postRun' in self.node:
+            self.data_zone[self.node['out']]=RunToolFunc.getFunc("bytes_to_list")(self.data_zone[self.node['out']])
+            for i in self.node['postRun']:
+                print('postRun: {}'.format(i))
+                self.data_zone[self.node['out']]=RunToolFunc.getFunc(i)(self.data_zone[self.node['out']])
+
+    def doRun(self):
+        self._preRun()
+        self._run()
+        self._postRun()
+
+class StartNode(Node):
+    def __init__(self,node):
+        super().__init__(node=node)
+
+class EndNode(Node):
+    def _run(self):
+        print("EndNode, Test Finish!")
+class ErrorNode(Node):
+    def __init__(self,data_zone):
+        self.data_zone=data_zone
+        self.node={
+            'id':'-'
+            ,'type':'error_node'
+            ,'next':'end'
+        }
+    
+    def __str__(self):
+        return super().__str__() + ", global_data : {}".format(self.data_zone)
+
+class ConditionNode(Node):
+    pass
+class ConstantContitionNode(ConditionNode):
+    def __init__(self,data_zone,node):
+        super().__init__(data_zone=data_zone,node=node)
+    
+    def _run(self):
+        # 查看跳转条件
+        for (key,value) in self.node['next'].items():
+            if self.data_zone[self.node['in']]==key:
+                self.data_zone[self.node['out']]=value
+                return
+        self.data_zone[self.node['out']]='error'
+
+class ComTestNode(Node):
+    def __init__(self,data_zone,node):
+        super().__init__(data_zone=data_zone,node=node)
+    
+    def _run(self):
+        self.data_zone[self.node['dev']].write(self.data_zone[self.node['in']])
+        # 等待数据准备完成
+        while self.data_zone[self.node['dev']].inWaiting() == 0:
+            time.sleep(0.005)
+        # 保证数据全部取出
+        byte_number_1 = 0
+        byte_number_2 = 1
+        while byte_number_1 != byte_number_2:
+            byte_number_1 = self.data_zone[self.node['dev']].inWaiting()
+            time.sleep(0.005)
+            byte_number_2 = self.data_zone[self.node['dev']].inWaiting()
+        # 一次性读取
+        self.data_zone[self.node['out']]=self.data_zone[self.node['dev']].read_all()
+
+class SendTpuNode(Node):
+    pass
+
+class ComInitNode(Node):
+    def __init__(self,data_zone,node):
+        super().__init__(data_zone=data_zone,node=node)
+    
+    def _run(self):
+        self.data_zone[self.node['out']]=serial.Serial(port=self.node['in']['port'],parity=self.node['in']['parity'],baudrate=self.node['in']['baudrate'])
+
+# 功能被ByteExtract代替
+# class TpuStatusExtract(Node):
+#     def __init__(self,data_zone,node):
+#         self.data_zone=data_zone
+#         self.node=node
+    
+#     def run(self):
+#         # 状态在报文第8个字节(从0开始数)
+#         self.data_zone[self.node['out']]='{:02X}'.format(self.data_zone[self.node['in']][8])
+
+# 获取指定位置的字节
+class ByteExtract(Node):
+    def __init__(self,data_zone,node):
+        super().__init__(data_zone=data_zone,node=node)
+    
+    def _run(self):
+        self.data_zone[self.node['out']]='{:02X}'.format(self.data_zone[self.node['in']][self.node['offset']])
+
+class NodeFactory:
+    def createNode(graph_node,global_data):
+        if graph_node['type']=='init_com':
+            return ComInitNode(data_zone=global_data,node=graph_node)
+        elif graph_node['type']=='com_node':
+            return ComTestNode(data_zone=global_data,node=graph_node)
+        elif graph_node['type']=='constant':
+            # 常量直接记录，不必建节点
+            global_data[graph_node['id']]=graph_node['data']
+        elif graph_node['type']=='start':
+            return StartNode(node=graph_node)
+        # elif graph_node['type']=='tpu_status_extract':
+        #     return TpuStatusExtract(data_zone=global_data,node=graph_node)
+        elif graph_node['type']=='end':
+            return EndNode()
+        elif graph_node['type']=='const_condition_node':
+            return ConstantContitionNode(data_zone=global_data,node=graph_node)
+        elif graph_node['type']=='byte_extract':
+            return ByteExtract(data_zone=global_data,node=graph_node)
+        else:
+            print('unsupport node,pass',graph_node)
+
+def run_test_graph(test_graph):
+    # 全局数据存储
     global_data=dict()
-    test_graph=[{'type':'init_com','id':'0','in':''},{'type':'test_node','id':'0','inDev':'$1'}]
+    # 运行节点
+    nodes=dict()
+    # 建图
     for i in test_graph:
-        for key,value in i.items():
-            print(key,":",value)
+        node = NodeFactory.createNode(i,global_data)
+        # 并非所有节点都需要运行
+        if isinstance(node,Node):
+            print("node: ",i)
+            nodes[i['id']]=node
+            if i['prev']=='start':
+                if 'start' in nodes:
+                    assert("duplicate start node, exit!")
+                nodes['start']=StartNode({
+                    'id':'start'
+                    ,'type':'start'
+                    ,'next':i['id']
+                })
+    # 其他边界节点
+    nodes['error']=ErrorNode(data_zone=global_data)
+    nodes['end']=EndNode()
+    currentNode = nodes['start']
+    while isinstance(currentNode,EndNode) != True:
+        print('running node',currentNode)
+        currentNode.doRun()
+        if isinstance(currentNode,ConditionNode):
+            currentNode=nodes[global_data[currentNode.node['out']]]
+        else:
+            currentNode=nodes[currentNode.node['next']]
+    currentNode.doRun()
+    print('finish global_data: {}'.format(global_data))
 
 if __name__ == '__main__':
-    ports = serial_ports()
-    print(ports)
-    # 020000001c800000001200003c0000be990000000c5000000000d10386070080908a03
-    beginData=bytes([0x02,0x00,0x00,0x00,0x1c,0x80,0x00,0x00,0x00,0x12,0x00,0x00,0x3c,0x00,0x00,0xbe,0x99,0x00,0x00,0x00,0x0c,0x50,0x00,0x00,0x00,0x00,0xd1,0x03,0x86,0x07,0x00,0x80,0x90,0x8a,0x03])
-    # or 
-    # beginData=b'\x02\x00\x00\x00\x1c\x80\x00\x00\x00\x12\x00\x00\x3c\x00\x00\xbe\x99\x00\x00\x00\x0c\x50\x00\x00\x00\x00\xd1\x03\x86\x07\x00\x80\x90\x8a\x03'
-    # if 'COM3' in ports:
-    #     result=send_str('COM3',115200,'N',beginData)
-    #     for i in result:
-    #         print(i)
-    
-    run_test_graph()
+    # print(model.graph)
+    run_test_graph(model.graph)
---
+++ idcard_sam_trans_model.py
@@ -0,0 +1,201 @@
+# 
+# tool _> tpu reset
+# tool <_ tpu 
+# tool _> tpu init (tool模拟一种设备)
+# tool <_ tpu
+# for
+# tool _> tpu type b search
+# tool <_ tpu (可能没有，循环等待)
+# end
+# for
+# SAM <_> tool <_> tpu
+# end
+graph = [
+    # 两个设备初始化
+    {
+        'type':'init_com'
+        ,'id':'sam'
+        ,'in': {
+            'baudrate':'115200'
+            ,'parity':'N'
+            ,'port':'COM3'
+        }
+        ,'out':'out_sam'
+        ,'prev':'start' # 标记起始
+        ,'next':'tpu'
+    }
+    ,{
+        'type':'init_com'
+        ,'id':'tpu'
+        ,'in':{
+            'baudrate':'115200'
+            ,'parity':'N'
+            ,'port':'COM5'
+        }
+        ,'out':'out_tpu'
+        ,'prev':'sam'
+        ,'next':'tpu_read'
+    }
+    # tpu读状态
+    ,{
+        'type':'constant'
+        ,'id':'tpu_read_code'
+        # 给tpu的数据，只写内容报文，校验和包头包尾可以使用preRun的pack_tpu自动添加
+        ,'data':[0x04,0x00,0x21,0x53,0x02,0x00]
+    }
+    ,{
+        'type':'com_node'
+        ,'id':'tpu_read'
+        ,'dev':'out_tpu'
+        ,'in': 'tpu_read_code'
+        ,'preRun':['pack_tpu']
+        ,'postRun':['unpack_tpu']
+        ,'out':'tpu_status'
+        ,'prev':'sam'
+        ,'next':'tpu_status_extract'
+    }
+    # tpu状态提取
+    ,{
+        'type':'byte_extract'
+        ,'id':'tpu_status_extract'
+        ,'in':'tpu_status'
+        ,'offset':6
+        ,'out':'tpu_status_short'
+        ,'prev':'tpu_read'
+        ,'next':'tpu_status_condition'
+    }
+    # 状态判断和跳转
+    ,{
+        'type':'const_condition_node'
+        ,'id':'tpu_status_condition'
+        ,'in':'tpu_status_short'
+        ,'out':'tpu_status_id'
+        ,'prev':'tpu_read'
+        ,'next':{
+            'FF':'tpu_reset'
+            ,'FE':'tpu_init'
+            ,'00':'tpu_read_bcard'
+        }
+    }
+    # tpu复位命令及运行
+    ,{
+        'type':'constant'
+        ,'id':'tpu_reset_code'
+        ,'data':[0x04,0x00,0x02,0x53,0x00,0x00]
+    }
+    ,{
+        'type':'com_node'
+        ,'id':'tpu_reset'
+        ,'dev':'out_tpu'
+        ,'in': 'tpu_reset_code'
+        ,'preRun':['pack_tpu']
+        ,'out':'out_tpu1'
+        ,'prev':'tpu_status_condition'
+        ,'next':'tpu_init'
+    }
+    # tpu初始化命令及运行
+    ,{
+        'type':'constant'
+        ,'id':'tpu_init_code'
+        ,'data':[0x02,0x31,0x00,0x01,0x53,0x00,0x00,0x00,0x00,0x00,0x10,0x02,0x17,0x59,0x01,0x3B,0x10,0x02,0x3B,0x11,0x00,0x09,0x39,0x00,0x00,0x00,0x20,0x22,0x07,0x05,0x20,0x22,0x07,0x05,0x00,0x01,0x00,0x33,0x33,0x33,0x00,0x11,0x10,0x03,0x00,0x05,0x00,0x10,0x03,0x00,0x01,0x00,0x00,0x00,0x00,0x00,0x2A,0x03]
+    }
+    ,{
+        'type':'com_node'
+        ,'id':'tpu_init'
+        ,'dev':'out_tpu'
+        ,'in':'tpu_init_code'
+        ,'out':'out_tpu2'
+        ,'prev':['tpu_status_condition','tpu_reset']
+        ,'next':'tpu_read_bcard'
+    }
+    # tpu 寻type-b卡
+    ,{
+        'type':'constant'
+        ,'id':'tpu_typeb_code'
+        ,'data':[0x0C,0x00,0x31,0x54,0x00,0x03,0x00,0x20,0x22,0x07,0x05,0x13,0x39,0x08]
+    }
+    ,{
+        'type':'com_node'
+        ,'id':'tpu_read_bcard'
+        ,'dev':'out_tpu'
+        ,'in': 'tpu_typeb_code'
+        ,'preRun':['pack_tpu']
+        ,'postRun':['unpack_tpu']
+        ,'out':'b_card_status'
+        ,'prev':['tpu_status_condition','tpu_init']
+        ,'next':'b_card_status_extract'
+    }
+    # 读卡状态提取
+    ,{
+        'type':'byte_extract'
+        ,'id':'b_card_status_extract'
+        ,'in':'b_card_status'
+        ,'offset':5
+        ,'out':'b_card_status_short'
+        ,'prev':'tpu_read_bcard'
+        ,'next':'b_card_status_condition'
+    }
+    # 读卡状态判断和跳转
+    ,{
+        'type':'const_condition_node'
+        ,'id':'b_card_status_condition'
+        ,'in':'b_card_status_short'
+        ,'out':'bcard_status_id'
+        ,'prev':'b_card_status_extract'
+        ,'next':{
+            '01':'send_to_sam'
+            ,'00':'tpu_read_bcard'
+        }
+    }
+    # 发SAM命令,第一次是固定的
+    ,{
+        'type':'constant'
+        ,'id':'command_to_sam'
+        ,'data':[0x02,0x00,0x00,0x00,0x1c,0x80,0x00,0x00,0x00,0x12,0x00,0x00,0x3c,0x00,0x00,0xbe,0x99,0x00,0x00,0x00,0x0c,0x50,0x00,0x00,0x00,0x00,0xd1,0x03,0x86,0x07,0x00,0x80,0x90,0x8a,0x03]
+    }
+    ,{
+        'type':'com_node'
+        ,'id':'send_to_sam'
+        ,'dev':'out_sam'
+        ,'in': 'command_to_sam'
+        ,'preRun':['log']
+        ,'postRun':['log']
+        ,'out':'recv_from_sam'
+        ,'prev':'b_card_status_condition'
+        ,'next':'sam_output_extract'
+    }
+    # 提取首字节用于判断是否完成通信
+    ,{
+        'type':'byte_extract'
+        ,'id':'sam_output_extract'
+        ,'in':'recv_from_sam'
+        ,'offset':0
+        ,'out':'recv_from_sam_short'
+        ,'prev':'send_to_sam'
+        ,'next':'tpu_sam_condition'
+    }
+    # sam返回状态判断和跳转
+    ,{
+        'type':'const_condition_node'
+        ,'id':'tpu_sam_condition'
+        ,'in':'recv_from_sam_short'
+        ,'out':'sam_tpu_status_id'
+        ,'prev':'sam_output_extract'
+        ,'next':{
+            'DD':'send_to_tpu' # 判断条件目前是大小写敏感的
+            ,'02':'end'
+        }
+    }
+    # sam数据发到tpu
+    ,{
+        'type':'com_node'
+        ,'id':'send_to_tpu'
+        ,'dev':'out_tpu'
+        ,'in': 'recv_from_sam'
+        ,'preRun':['log','sam_to_tpu','log','pack_tpu','log'] # 按顺序执行各个预处理
+        ,'postRun':['log','unpack_tpu','log','tpu_to_sam','log'] # 按顺序执行各个后处理
+        ,'out':'command_to_sam'
+        ,'prev':'tpu_sam_condition'
+        ,'next':'send_to_sam'
+    }
+]
